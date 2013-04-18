7d3012f58e40234a453d3ddee7ed9eaed948cff4
Kenny Cason <kenneth.cason@gmail.com>
2013-04-18 09:55:32 -0500

    Solved 18 & 67

---
+++ src/lib/DynamicAlgorithms.java
@@ -0,0 +1,40 @@
+package lib;
+
+public class DynamicAlgorithms {
+	
+	private DynamicAlgorithms() {
+	}
+	
+	public static int getMaxPath(IntTriangle pathSums) {
+		int max = Integer.MIN_VALUE;
+		int y = pathSums.height() - 1;
+		for(int x = 0; x < pathSums.height(); x++) {
+			if(pathSums.get(y, x) > max) {
+				max = pathSums.get(y, x);
+			}
+		}
+		return max;
+	}
+
+	public static IntTriangle calculateMaxPaths(IntTriangle triangle) {
+		IntTriangle pathSums = new IntTriangle(triangle.height());
+		// init
+		pathSums.set(0, 0, triangle.get(0, 0));
+		
+		// node1
+		for(int y = 0; y < triangle.height() - 1; y++) {
+			for(int x = 0; x < triangle.row(y).length; x++) {
+				// node 2
+				int val = pathSums.get(y, x) + triangle.get(y + 1, x);
+				if(val > pathSums.get(y + 1, x)) {
+					pathSums.set(y + 1, x, val);
+				}
+				val = pathSums.get(y, x) + triangle.get(y + 1, x + 1);
+				if(val > pathSums.get(y + 1, x + 1)) {
+					pathSums.set(y + 1, x + 1, val);
+				}
+			}
+		}
+		return pathSums;
+	}
+}
---
+++ src/lib/IntTriangle.java
@@ -0,0 +1,60 @@
+package lib;
+
+public class IntTriangle {
+
+	private int height;
+	
+	private int numPoints;
+	
+	private int[][] triangle;
+	
+	public IntTriangle(int height) {
+		this.height = height;
+		triangle = new int[height][];
+		for(int y = 0; y < height; y++) {
+			triangle[y] = new int[y + 1];
+		}
+		numPoints = (int) Numbers.factorial(height);
+	}
+	
+	public void setAll(int... values) {
+		int i = 0;
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < triangle[y].length; x++) {
+				triangle[y][x] = values[i];
+				i++;
+			}
+		}
+	}
+	
+	public void set(int y, int x, int value) {
+		triangle[y][x] = value;
+	}
+	
+	public int get(int y, int x) {
+		return triangle[y][x];
+	}
+	
+	public int[] row(int y) {
+		return triangle[y];
+	}
+	
+	public int height() {
+		return height;
+	}
+	
+	public int numPoints() {
+		return numPoints;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < triangle[y].length; x++) {
+				sb.append(triangle[y][x] + "  ");
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+}
