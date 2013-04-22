9fa580bb88092dbdf9b5f763d7579bfd23007563
Kenny Cason <kenneth.cason@gmail.com>
2013-04-22 03:54:23 -0500

    solved #30

---
+++ src/_030/_030.java
@@ -0,0 +1,41 @@
+package _030;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import lib.Numbers;
+
+
+
+public class _030 {
+
+	public static void main(String[] args) {
+		new _030();
+	}
+
+	public _030() {
+		int sum = 0;
+		for(int i = 2; i < 250000; i++)  {
+			if(match(i)) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+	}
+	
+	
+	private boolean match(int i) {
+		int sum = 0;
+		int orig = i;
+		int digit;
+		do {
+			digit = i % 10;
+			sum += digit * digit * digit * digit * digit;
+			i /= 10;
+		} while(i > 0);
+		return orig == sum;
+	}
+
+}
